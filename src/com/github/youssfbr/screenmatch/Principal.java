8f951294ba2ee7f00528c5f2e183a9ac064339ff
Alisson Youssf <youssfbr@gmail.com>
2024-10-15 21:22:13 -0300

    Adicionando filmes na Lista com ArrayList

--- src/com/github/youssfbr/screenmatch/Principal.java
+++ src/com/github/youssfbr/screenmatch/Principal.java
@@ -6,6 +6,8 @@ import com.github.youssfbr.screenmatch.modelos.Episodio;
 import com.github.youssfbr.screenmatch.modelos.Filme;
 import com.github.youssfbr.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
 
@@ -67,5 +69,25 @@ public class Principal {
         episodio.setSerie(lost);
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
+
+        var filmeDoPaulo = new Filme();
+        filmeDoPaulo.setNome("Dogville");
+        filmeDoPaulo.setDuracaoEmMinutos(200);
+        filmeDoPaulo.setAnoDeLancamento(2003);
+        filmeDoPaulo.avalia(10);
+
+        final ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoPaulo);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+
+        System.out.println("\n----------------------------------------------\n");
+        final String s = listaDeFilmes.size() > 1 ? "s" : "";
+        System.out.println("Tamanho da Lista: " + listaDeFilmes.size() + " filme" + s);
+        System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
+        System.out.println("Primeiro filme: " + listaDeFilmes.getFirst().getNome());
+
+        System.out.println("\n----------------------------------------------\n");
+        System.out.println(listaDeFilmes);
     }
 }
